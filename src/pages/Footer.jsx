2e56e19f9300ec8089014abba9dda832afac1731
dip_sarker <sarkerdip005@gmail.com>
2025-03-10 10:56:57 +0600

    update imagepayment

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,56 +1,68 @@
-import React from 'react'
-import '../assets/css/Footer.css'
+
+
+
+
+import React from 'react';
+import '../assets/css/Footer.css';
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import bimage from "../assets/img/sslcommerz-banner.png"
+
 
 export const Footer = () => {
   return (
     <div>
- <footer className="footer bg-dark mt-auto text-light py-4">
-      <div className="container">
-        <div className="row">
-          {/* About Section */}
-          <div className="col-md-4">
-            <h5>About Us</h5>
-            <p>
-              Vehicle Parking Management System helps you manage parking spaces
-              efficiently with real-time tracking.
-            </p>
-          </div>
+      <footer className="footer bg-dark mt-auto text-light py-4">
+        <div className="container">
+          <div className="row">
+            {/* About Section */}
+            <div className="col-md-4">
+              <h5>About Us</h5>
+              <p>
+                Vehicle Parking Management System helps you manage parking spaces efficiently with real-time tracking.
+              </p>
+            </div>
 
-          {/* Quick Links */}
-          <div className="col-md-4">
-            <h5>Quick Links</h5>
-            <ul className=" list-unstyled">
-              <Link to={"/" }><li  className="text-light">Home</li></Link>
-              <Link to={"/about"}><li className="text-light">About</li></Link>
-              <Link to={"/service"}><li  className="text-light">Services</li></Link>
-              <Link to={"/contact"}><li  className="text-light">Contact</li></Link>
-            </ul>
-          </div>
+            {/* Quick Links */}
+            <div className="col-md-4">
+              <h5>Quick Links</h5>
+              <ul className="list-unstyled">
+                <Link to={"/"}><li className="text-light">Home</li></Link>
+                <Link to={"/about"}><li className="text-light">About</li></Link>
+                <Link to={"/service"}><li className="text-light">Services</li></Link>
+                <Link to={"/contact"}><li className="text-light">Contact</li></Link>
+              </ul>
+            </div>
 
-          {/* Contact Section */}
-          <div className="col-md-4">
-            <h5>Contact Us</h5>
-            <p>Email: support@parking.com</p>
-            <p>Phone: +8801303053626</p>
-            <div className="social-icons">
-             <Link> <a  className="text-light me-2"><FaFacebook size={20} /></a></Link>
-             <Link> <a  className="text-light me-2"><FaTwitter size={20} /></a></Link>
-             <Link> <a className="text-light"><FaInstagram size={20} /></a></Link>
+            {/* Contact Section */}
+            <div className="col-md-4">
+              <h5>Contact Us</h5>
+              <p>Email: support@parking.com</p>
+              <p>Phone: +8801303053626</p>
+              <div className="social-icons">
+                <Link><a className="text-light me-2"><FaFacebook size={20} /></a></Link>
+                <Link><a className="text-light me-2"><FaTwitter size={20} /></a></Link>
+                <Link><a className="text-light"><FaInstagram size={20} /></a></Link>
+              </div>
             </div>
           </div>
+
+         
+          <div className="text-center mt-4">
+            <img 
+              src={bimage}
+              alt="SSLCommerz Payment Gateway" 
+              className="img-fluid" 
+              style={{ maxWidth: "500px" }} 
+            />
+          </div>
         </div>
-      </div>
 
-      {/* Copyright */}
-      <div className="text-center mt-3">
-        <p className="mb-0">&copy; {new Date().getFullYear()} Vehicle Parking System. All rights reserved.</p>
-      </div>
-    </footer>
-
-
-
+        {/* Copyright */}
+        <div className="text-center mt-3">
+          <p className="mb-0">&copy; {new Date().getFullYear()} Vehicle Parking System. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
